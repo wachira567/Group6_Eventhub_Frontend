f28eb5449864972b13770ff9ff8f51e2fdcc17cd
Fainus Gabriel <nusgabriel17@gmail.com>
2026-02-03 14:28:28 +0300

    basic imports

---
+++ src/pages/admin/EventModeration.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Search, CheckCircle, XCircle, Eye, ChevronLeft, Calendar, User, MapPin, AlertTriangle, Filter, Loader2 } from 'lucide-react';
+import { Button } from '../../components/ui/button';
+import { formatDate, formatCurrency } from '../../utils/helpers';
+import { API_BASE_URL } from '../../utils/constants';
+
+const EventModeration = () => {
+  return <div>Event Moderation Component</div>;
+};
+
+export default EventModeration;
