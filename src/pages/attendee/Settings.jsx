edf652a8a1be08a0edecd884b3a9830a27f2cca9
Faisal Zaid <zaidalideveloper@gmail.com>
2026-02-04 08:15:41 +0300

    added imports

---
+++ src/pages/attendee/Settings.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link, useLocation } from 'react-router-dom';
+import { Ticket, Heart, User, Settings, Loader2, Save, Eye, EyeOff } from 'lucide-react';
+import { Button } from '../../components/ui/button';
+import { Input } from '../../components/ui/input';
+import { Label } from '../../components/ui/label';
+import { API_BASE_URL } from '../../utils/constants';
+import { updateUser } from '../../store/slices/authSlice';
+import { toast } from 'sonner';
