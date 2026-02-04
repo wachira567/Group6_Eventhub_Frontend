a45ea8aea337761c310baf91e793a77582619549
habiba hussein <habiba.hussein@moringaschool.comclear>
2026-02-04 11:01:46 +0300

    Refactor(Home): Add initial imports and setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import HeroSection from '../sections/HeroSection';
+import CategoriesSection from '../sections/CategoriesSection';
+import FeaturedEventsSection from '../sections/FeaturedEventsSection';
+import PopularCitiesSection from '../sections/PopularCitiesSection';
+import HowItWorksSection from '../sections/HowItWorksSection';
+import CTASection from '../sections/CTASection';
+
+const Home = () => {
+  return (
+    <div className="animate-fade-in">
