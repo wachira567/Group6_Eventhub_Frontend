1b6599e617ab67985e36927d4baa34340c7d7070
Moses Maina <mainacalton57@gmail.com>
2026-02-04 12:12:33 +0300

    implement Separator component for layout spacing

---
+++ src/components/ui/separator.jsx
@@ -0,0 +1,28 @@
+"use client"
+
+import * as React from "react"
+import * as SeparatorPrimitive from "@radix-ui/react-separator"
+
+import { cn } from "@/lib/utils"
+
+function Separator({
+  className,
+  orientation = "horizontal",
+  decorative = true,
+  ...props
+}{
+  return (
+    <SeparatorPrimitive.Root
+      data-slot="separator"
+      decorative={decorative}
+      orientation={orientation}
+      className={cn(
+        "bg-border shrink-0 data-[orientation=horizontal],
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+export { Separator }
