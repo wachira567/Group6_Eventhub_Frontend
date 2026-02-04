11392317e8f0a3a827151d74bb7883a3e046d8d7
Moses Maina <mainacalton57@gmail.com>
2026-02-04 12:16:18 +0300

    changes made on Toggle button component

--- src/components/ui/toggle.jsx
+++ src/components/ui/toggle.jsx
@@ -1,31 +1,36 @@
 import * as React from "react"
 import * as TogglePrimitive from "@radix-ui/react-toggle"
-import { cva } from "class-variance-authority"
+import { cva,  } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 
 const toggleVariants = cva(
-  "inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium hover:shadow-md aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium hover,box-shadow] aria-invalid,
   {
-    variants: {
-      variant: {
-        default: "",
-        outline: "border border-input bg-transparent shadow-xs hover:shadow-sm",
+    variants{
+      variant{
+        default,
+        outline
+          "border border-input bg-transparent shadow-xs hover,
       },
-      size: {
-        default: "h-9 px-3",
-        sm: "h-8 px-2",
-        lg: "h-10 px-4",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
-    },
-  }
+      size{
+        default,
+        sm,
+        lg,
+      }, },
+    defaultVariants{
+      variant,
+      size,
+    }, }
 )
 
-function Toggle({ className, variant, size, ...props }) {
+function Toggle({
+  className,
+  variant,
+  size,
+  ...props
+}
+  VariantProps<typeof toggleVariants>) {
   return (
     <TogglePrimitive.Root
       data-slot="toggle"
